e69eaa72268bb3c3f2116469b7b637908a6b62cb
Leo Baker-Hytch <l.bakerhytch@gmail.com>
2015-11-14 17:53:35 +0000

    Rewrites validity tests in terms of instatiation of ‘Puzzle’

--- bridges/test_puzzle.py
+++ bridges/test_puzzle.py
@@ -1,48 +1,54 @@
 import unittest
 
-from puzzle import is_valid_puzzle
+from puzzle import Puzzle
 
 class ValidityTestCase(unittest.TestCase):
-    """Tests for `is_valid_puzzle` in `io.py`."""
+    """Tests for instantiation of Puzzle."""
 
     def test_reject_empty_puzzle(self):
         """Is an empty puzzle rejected?"""
-        P = []
-        self.assertFalse(is_valid_puzzle(P))
+        p = []
+        with self.assertRaises(ValueError):
+            Puzzle(p)
 
     def test_reject_mismatched_row_lengths(self):
         """Is a puzzle with mismatched row lengths rejected?"""
-        P = [[1, 0], [0, 0, 1]]
-        self.assertFalse(is_valid_puzzle(P))
+        p = [[1, 0], [0, 0, 1]]
+        with self.assertRaises(ValueError):
+            Puzzle(p)
 
     def test_reject_invalid_numbers(self):
         """Is a puzzle with numbers other than 0–8 rejected?"""
-        Ps = [ [[9]], [[10]], [[-1]] ]
-        for P in Ps:
-            self.assertFalse(is_valid_puzzle(P))
+        ps = [ [[9]], [[10]], [[-1]] ]
+        for p in ps:
+            with self.assertRaises(ValueError):
+                Puzzle(p)
 
     def test_accept_valid_numbers(self):
         """Is a puzzle with only valid numbers accepted?"""
-        P = [[1, 0, 2, 0, 3, 0, 4, 0], [0, 5, 0, 6, 0, 7, 0, 8]]
-        self.assertTrue(is_valid_puzzle(P))
+        p = [[1, 0, 2, 0, 3, 0, 4, 0], [0, 5, 0, 6, 0, 7, 0, 8]]
+        Puzzle(p)
 
     def test_reject_horizontally_adjacent_islands(self):
         """Is a puzzle with horizontally-adjacent islands rejected?"""
-        Ps = [ [[1, 1]], [[0, 0], [1, 1]] ]
-        for P in Ps:
-            self.assertFalse(is_valid_puzzle(P))
+        ps = [ [[1, 1]], [[0, 0], [1, 1]] ]
+        for p in ps:
+            with self.assertRaises(ValueError):
+                Puzzle(p)
 
     def test_reject_vertically_adjacent_islands(self):
         """Is a puzzle with vertically-adjacent islands rejected?"""
-        Ps = [ [[1, 0], [1, 0]], [[0, 0, 0], [0, 1, 0], [0, 1, 0]] ]
-        for P in Ps:
-            self.assertFalse(is_valid_puzzle(P))
+        ps = [ [[1, 0], [1, 0]], [[0, 0, 0], [0, 1, 0], [0, 1, 0]] ]
+        for p in ps:
+            with self.assertRaises(ValueError):
+                Puzzle(p)
 
     def test_accept_valid_puzzle(self):
         """Is a valid puzzle accepted?"""
-        Ps = [ [[1]], [[1, 0]], [[1, 0, 1], [0, 1, 0]] ]
-        for P in Ps:
-            self.assertTrue(is_valid_puzzle(P))
+        ps = [ [[1]], [[1, 0]], [[1, 0, 1], [0, 1, 0]] ]
+        for p in ps:
+            with self.assertRaises(ValueError):
+                Puzzle(p)
 
 
 if __name__ == '__main__':
